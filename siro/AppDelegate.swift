99c36b292fbb44a8464811b7b3961b178a55a1ec
Naoki Ainoya <nainoya@zlab.co.jp>
2019-11-27 15:03:24 +0900

    Initial Commit

---
+++ siro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  siro
+//
+//  Created by nainoya on 2019/11/27.
+//  Copyright © 2019 ainoya.tokyo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ siro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  siro
+//
+//  Created by nainoya on 2019/11/27.
+//  Copyright © 2019 ainoya.tokyo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
